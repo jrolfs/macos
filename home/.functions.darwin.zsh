0d74fbd7f35fd18120c489dd314d9f0197c43d9e
Jamie Rolfs <jamie.rolfs@gmail.com>
2017-04-27 06:10:27 -0700

    Add function for resetting hostname on network

---
+++ home/.functions.darwin.zsh
@@ -0,0 +1,13 @@
+function reset-host {
+  host=$(hostname)
+
+  echo "Setting HostName to ${host}"
+  sudo scutil --set HostName $host
+  echo "Setting LocalHostName to ${host}"
+  sudo scutil --set LocalHostName $host
+  echo "Setting ComputerName to ${host}"
+  sudo scutil --set ComputerName $host
+
+  echo "Flushing DNS cache..."
+  sudo killall -HUP mDNSResponder
+}
