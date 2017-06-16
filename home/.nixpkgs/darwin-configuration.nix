93f5addc4364a5b227c2345abc4896619d3b567f
Jamie Rolfs <jamie.rolfs@gmail.com>
2017-06-16 04:28:23 -0700

    Add nix-darwin configuration

---
+++ home/.nixpkgs/darwin-configuration.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.systemPackages =
+    [ pkgs.figlet
+      pkgs.fzf
+      pkgs.git
+      pkgs.git-crypt
+      pkgs.gnupg
+      pkgs.go
+      pkgs.irssi
+      pkgs.jq
+      pkgs.lame
+      pkgs.mosh
+      pkgs.nix
+      pkgs.perl
+      pkgs.tmux
+      pkgs.zsh
+
+      pkgs.nix
+      pkgs.nix-repl ];
+
+  programs.zsh.enable = true;
+
+  services.activate-system.enable = true;
+}
