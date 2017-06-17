547d775e5b33dd7ba673fad905bea8c396833078
Jamie Rolfs <jamie.rolfs@gmail.com>
2017-06-17 11:47:44 -0700

    Set up nix-darwin environment manually

--- home/.darwin.zsh
+++ home/.darwin.zsh
@@ -1,8 +1,25 @@
+#
 # nix-darwin
 
 nix_darwin_init=/etc/static/zshrc
-[ -f $nix_darwin_init ] && source $nix_darwin_init
 
+if [ -f $nix_darwin_init ]; then
+  export NIX_CONF_DIR="/etc/nix"
+  export NIX_OTHER_STORES="/run/nix/remote-stores/*/nix"
+  export NIX_PATH="darwin=$HOME/.nix-defexpr/darwin:darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:nixpkgs=$HOME/.nix-defexpr/channels/nixpkgs:$HOME/.nix-defexpr/channels"
+  export NIX_USER_PROFILE_DIR="/nix/var/nix/profiles/per-user/$USER"
+  export NIX_PROFILES="/nix/var/nix/profiles/default /run/current-system/sw $HOME/.nix-profile"
+
+  export NIX_SSL_CERT_FILE="$(grep -o '".*\.crt"' $nix_darwin_init | tr -d '"')"
+
+  path+=(
+    /run/current-system/sw/bin
+    /nix/var/nix/profiles/default/bin
+    $HOME/.nix-profile/bin
+  )
+fi
+
+#
 # Functions
 
 function reset-host {
