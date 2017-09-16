8983ff7ba752419b45588ee644a3f44da34033f5
Jamie Rolfs <jamie.rolfs@gmail.com>
2017-09-16 08:42:27 -0700

    Add oni config

---
+++ home/.oni/config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  "oni.useDefaultConfig": false,
+  "oni.loadInitVim": true,
+
+  "oni.useExternalPopupMenu": true,
+
+  "oni.hideMenu": false,
+
+  "oni.exclude": ["**/node_modules/**"],
+  "oni.bookmarks": [],
+
+  "editor.backgroundOpacity": 1.0,
+  "editor.backgroundImageUrl": null,
+  "editor.backgroundImageSize": "initial",
+
+  "editor.clipboard.enabled": false,
+
+  "editor.quickInfo.enabled": true,
+  "editor.quickInfo.delay": 500,
+
+  "editor.completions.enabled": true,
+  "editor.errors.slideOnFocus": true,
+  "editor.formatting.formatOnSwitchToNormalMode": false,
+
+  "editor.fontLigatures": true,
+  "editor.fontSize": "12px",
+  "editor.fontFamily": "FuraCode Nerd Font",
+
+  "editor.quickOpen.execCommand": null,
+
+  "editor.scrollBar.visible": false,
+
+  "editor.fullScreenOnStart" : false,
+
+  "editor.cursorLine": true,
+  "editor.cursorLineOpacity" : 0.1,
+
+  "editor.cursorColumn": false,
+  "editor.cursorColumnOpacity": 0.1,
+
+  "environment.additionalPaths": [],
+
+  "statusbar.enabled": true,
+  "statusbar.fontSize": "12px",
+
+  "tabs.enabled": true,
+  "tabs.showVimTabs": true,
+};
