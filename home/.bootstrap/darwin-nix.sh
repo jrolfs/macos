2efb3f61073b13202521018bc6da358cec3071c9
Jamie Rolfs <jamie.rolfs@gmail.com>
2017-10-03 14:40:36 -0700

    Update NIX_PATH in darwin-nix bootstrap script

--- home/.bootstrap/darwin-nix.sh
+++ home/.bootstrap/darwin-nix.sh
@@ -2,7 +2,11 @@
 
 [ ! -L /run ] && sudo ln -s private/var/run /run
 
-export NIX_PATH=darwin=$HOME/.nix-defexpr/darwin:darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:$NIX_PATH
+export NIX_PATH=darwin=$HOME/.nix-defexpr/darwin:\
+nixpkgs=$HOME/.nix-defexpr/nixpkgs:\
+darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:\
+/nix/var/nix/profiles/per-user/$USER/channels:\
+$NIX_PATH
 
 $(nix-build '<darwin>' -A system --no-out-link)/sw/bin/darwin-rebuild build
 $(nix-build '<darwin>' -A system --no-out-link)/sw/bin/darwin-rebuild switch
