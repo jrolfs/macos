92446e1d8041b7de675ba8d78960cf8ea5b7d2c4
Jamie Rolfs <jamie.rolfs@gmail.com>
2018-10-28 01:31:20 -0700

    Move Nix overlays to separate module

--- home/.nixpkgs/darwin-configuration.nix
+++ home/.nixpkgs/darwin-configuration.nix
@@ -1,17 +1,7 @@
+overlays = import ./overlays.nix
+
 { config, lib, pkgs, ... }:
 
-let
-  overlays = self: super: rec {
-    chunkwm = super.recurseIntoAttrs (super.callPackage (super.fetchFromGitHub {
-      owner = "kubek2k";
-      repo = "chunkwm.nix";
-      sha256 = "11fwr29q18x4349wdg1pd7wqd1wvxsib6mjz7c93slf40h88vd53";
-      rev = "0.1";
-    }) {
-      inherit (super.darwin.apple_sdk.frameworks) Carbon Cocoa ApplicationServices;
-    });
-  };
-in
 {
   # macOS Settings
 
---
+++ home/.nixpkgs/overlays.nix
@@ -0,0 +1,10 @@
+self: super: rec {
+  chunkwm = super.recurseIntoAttrs (super.callPackage (super.fetchFromGitHub {
+    owner = "kubek2k";
+    repo = "chunkwm.nix";
+    sha256 = "11fwr29q18x4349wdg1pd7wqd1wvxsib6mjz7c93slf40h88vd53";
+    rev = "0.1";
+  }) {
+    inherit (super.darwin.apple_sdk.frameworks) Carbon Cocoa ApplicationServices;
+  });
+};
