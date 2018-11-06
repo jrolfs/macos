4aa0e32f5eefcfd1a5f733666ddeda748a4ac038
Jamie Rolfs <jamie.rolfs@gmail.com>
2018-11-06 10:02:32 -0800

    nix: point chunkwm overlay at my fork

--- home/.nixpkgs/overlays.nix
+++ home/.nixpkgs/overlays.nix
@@ -2,9 +2,9 @@ self: super:
 
 rec {
   chunkwm = super.recurseIntoAttrs (super.callPackage (super.fetchFromGitHub {
-    owner = "kubek2k";
+    owner = "jrolfs";
     repo = "chunkwm.nix";
-    sha256 = "11fwr29q18x4349wdg1pd7wqd1wvxsib6mjz7c93slf40h88vd53";
+    sha256 = "6a21e807785515a1f3607f5e65a4adbe641010db";
     rev = "0.1";
   }) {
     inherit (super.darwin.apple_sdk.frameworks) Carbon Cocoa ApplicationServices;
