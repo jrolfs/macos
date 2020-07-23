4c35c2ea0279115ad287904bacd48f0b59d8c443
Jamie Rolfs <jamie.rolfs@hover.to>
2020-07-23 08:27:42 -0700

    icons: add super sofisticated icon apply script

---
+++ icons/apply.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+echo "\n\n\n$(date -u +"%Y-%m-%dT%H:%M:%SZ") / $(date)\n---------------------------------------------------" >> apply.log
+
+fd --type=f '.*' ./assets --exec zsh -c 'fileicon set /Applications/$2.app $1 &>> apply.log && echo "✅ $2" || echo "❌ $2"' zsh {} {/.}
