c9f88192dcff5c80f7ac1df8a6ab1ff502e79d36
Jamie Rolfs <jamie.rolfs@hover.to>
2020-08-14 14:25:23 -0700

    icons: add/update some assets... attempt to set /System icons

--- icons/apply.sh
+++ icons/apply.sh
@@ -3,3 +3,5 @@
 echo "\n\n\n$(date -u +"%Y-%m-%dT%H:%M:%SZ") / $(date)\n---------------------------------------------------" >> apply.log
 
 fd --type=f '.*' ./assets --exec zsh -c 'fileicon set /Applications/$2.app $1 &>> apply.log && echo "✅ $2" || echo "❌ $2"' zsh {} {/.}
+
+sudo fd --type=f '.*' ./assets/System --exec zsh -c 'fileicon set /System/Applications/$2.app $1 &>> apply.log && echo "✅ $2" || echo "❌ $2"' zsh {} {/.}
