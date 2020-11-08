83b025e36df2e4bee716d8b3a5b442cf670393a5
Jamie Rolfs <jamie.rolfs@hover.to>
2020-11-08 09:30:46 -0800

    nix: pin neovim overlay to a commit before the tree-sitter de-vendor

--- home/.nixpkgs/overlays.nix
+++ home/.nixpkgs/overlays.nix
@@ -5,6 +5,7 @@ rec {
     version = "master";
     src = builtins.fetchGit {
       url = https://github.com/neovim/neovim.git;
+      rev = "4d9e5764d243920122794e103a07814783b57d28";
     };
   });
 
