15082992b868da84c7de66cb783b83c807afd5ed
Jamie Rolfs <jamie.rolfs@hover.to>
2021-08-12 18:54:40 -0700

    stay: add scripts to set Moom window gap when switching displays

---
+++ home/.config/stay/action-1440x900.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# Set window gap in Moom to 10px
+defaults write /Users/jamie/Library/Preferences/com.manytricks.Moom "Grid Spacing: Gap" -int 10
+
---
+++ home/.config/stay/action-2560x1440.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# Set window gap in Moom to 20px
+defaults write /Users/jamie/Library/Preferences/com.manytricks.Moom "Grid Spacing: Gap" -int 20
+
