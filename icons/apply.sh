b449fbacec02ca80c79b022ce8c82fbd32293413
Jamie Rolfs <jamie.rolfs@hover.to>
2021-08-31 12:19:08 -0700

    icons: don't try to apply system icons... that's a whole thing on Big Sur

--- icons/apply.sh
+++ icons/apply.sh
@@ -4,4 +4,4 @@ echo "\n\n\n$(date -u +"%Y-%m-%dT%H:%M:%SZ") / $(date)\n------------------------
 
 fd --type=f '.*' ./assets --exec zsh -c 'fileicon set /Applications/$2.app $1 &>> apply.log && echo "✅ $2" || echo "❌ $2"' zsh {} {/.}
 
-sudo fd --type=f '.*' ./assets/System --exec zsh -c 'fileicon set /System/Applications/$2.app $1 &>> apply.log && echo "✅ $2" || echo "❌ $2"' zsh {} {/.}
+# sudo fd --type=f '.*' ./assets/System --exec zsh -c 'fileicon set /System/Applications/$2.app $1 &>> apply.log && echo "✅ $2" || echo "❌ $2"' zsh {} {/.}
