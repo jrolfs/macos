248a160aaa9690cd816089373bc4586e7ec194c5
Jamie Rolfs <jamie.rolfs@gmail.com>
2021-09-18 01:49:01 -0700

    nix: move defaults into separate section, hide menu bar

--- home/.nixpkgs/darwin-configuration.nix
+++ home/.nixpkgs/darwin-configuration.nix
@@ -5,66 +5,9 @@ let
 in
 {
   imports = [
+    ./defaults.nix
     ./homebrew.nix
   ];
-
-  # macOS Settings
-
-  system.defaults = {
-    dock = {
-      autohide = true;
-      orientation = "bottom";
-      showhidden = true;
-      minimize-to-application = true;
-      mineffect = "scale";
-      launchanim = true;
-      show-process-indicators = true;
-      tilesize = 48;
-      static-only = false;
-      mru-spaces = false;
-    };
-    finder = {
-      AppleShowAllExtensions = true;
-      FXEnableExtensionChangeWarning = false;
-      QuitMenuItem = true;
-    };
-    trackpad = {
-      Clicking = true;
-      TrackpadRightClick = true;
-      TrackpadThreeFingerDrag = true;
-    };
-    NSGlobalDomain = {
-      "com.apple.mouse.tapBehavior" = 1;
-      "com.apple.springing.delay" = "0.0";
-      "com.apple.springing.enabled" = true;
-      "com.apple.swipescrolldirection" = true;
-      "com.apple.trackpad.enableSecondaryClick" = true;
-      "com.apple.trackpad.trackpadCornerClickBehavior" = 1;
-      AppleFontSmoothing = 0;
-      AppleKeyboardUIMode = 3;
-      ApplePressAndHoldEnabled = false;
-      AppleShowAllExtensions = true;
-      AppleShowScrollBars = "WhenScrolling";
-      InitialKeyRepeat = 15;
-      KeyRepeat = 2;
-      NSAutomaticCapitalizationEnabled = false;
-      NSAutomaticDashSubstitutionEnabled = false;
-      NSAutomaticPeriodSubstitutionEnabled = false;
-      NSAutomaticQuoteSubstitutionEnabled = false;
-      NSAutomaticSpellingCorrectionEnabled = true;
-      NSDisableAutomaticTermination = true;
-      NSDocumentSaveNewDocumentsToCloud = false;
-      NSNavPanelExpandedStateForSaveMode = true;
-      NSNavPanelExpandedStateForSaveMode2 = true;
-      NSScrollAnimationEnabled = true;
-      NSTableViewDefaultSizeMode = 2;
-      NSTextShowsControlCharacters = false;
-      NSUseAnimatedFocusRing = true;
-      NSWindowResizeTime = "0.01";
-      PMPrintingExpandedStateForPrint = true;
-      PMPrintingExpandedStateForPrint2 = true;
-    };
-  };
 
   # Packages
 
---
+++ home/.nixpkgs/defaults.nix
@@ -0,0 +1,63 @@
+{ config, lib, ... }:
+
+{
+  system.defaults = {
+    dock = {
+      autohide = true;
+      launchanim = true;
+      mineffect = "scale";
+      minimize-to-application = true;
+      mru-spaces = false;
+      orientation = "bottom";
+      show-process-indicators = true;
+      showhidden = true;
+      static-only = false;
+      tilesize = 48;
+    };
+
+    finder = {
+      AppleShowAllExtensions = true;
+      FXEnableExtensionChangeWarning = false;
+      QuitMenuItem = true;
+    };
+
+    trackpad = {
+      Clicking = true;
+      TrackpadRightClick = true;
+      TrackpadThreeFingerDrag = true;
+    };
+
+    NSGlobalDomain = {
+      "com.apple.mouse.tapBehavior" = 1;
+      "com.apple.springing.delay" = "0.0";
+      "com.apple.springing.enabled" = true;
+      "com.apple.swipescrolldirection" = true;
+      "com.apple.trackpad.enableSecondaryClick" = true;
+      "com.apple.trackpad.trackpadCornerClickBehavior" = 1;
+      AppleFontSmoothing = 0;
+      AppleKeyboardUIMode = 3;
+      ApplePressAndHoldEnabled = false;
+      AppleShowAllExtensions = true;
+      AppleShowScrollBars = "WhenScrolling";
+      InitialKeyRepeat = 15;
+      KeyRepeat = 2;
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = true;
+      NSDisableAutomaticTermination = true;
+      NSDocumentSaveNewDocumentsToCloud = false;
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+      NSScrollAnimationEnabled = true;
+      NSTableViewDefaultSizeMode = 2;
+      NSTextShowsControlCharacters = false;
+      NSUseAnimatedFocusRing = true;
+      NSWindowResizeTime = "0.01";
+      PMPrintingExpandedStateForPrint = true;
+      PMPrintingExpandedStateForPrint2 = true;
+      _HIHideMenuBar = true;
+    };
+  };
+}
