4bd58b1b88ad8936ebaf689b767cb68bc3341c40
Jamie Rolfs <jamie.rolfs@gmail.com>
2024-06-16 10:16:39 -0700

    hammerspoon: move focus hide into init

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -21,3 +21,27 @@ end
 
 -- Bind the function to a hotkey, e.g., Cmd + Shift + C
 hs.hotkey.bind({ "ctrl", "alt", "cmd", "shift" }, "C", copyFinderPath)
+
+local appsToHide = {"kitty"} -- List of applications to hide
+
+-- Function to check if an application is in the list
+local function shouldHideApp(appName)
+    for _, name in ipairs(appsToHide) do
+        if name == appName then
+            return true
+        end
+    end
+    return false
+end
+
+-- Watcher function that gets called when the active application changes
+local appWatcher = hs.application.watcher.new(function(appName, eventType, appObject)
+    if eventType == hs.application.watcher.deactivated then
+        if shouldHideApp(appName) then
+            appObject:hide()
+        end
+    end
+end)
+
+-- Start the application watcher
+appWatcher:start()
