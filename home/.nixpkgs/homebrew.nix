fe6decaf1be03fbfb1552294df194200c0d5751d
Jamie Rolfs <jamie.rolfs@gmail.com>
2026-01-31 11:20:48 -0800

    nix(mas): add Flighty

--- home/.nixpkgs/homebrew.nix
+++ home/.nixpkgs/homebrew.nix
@@ -45,8 +45,10 @@ in
   ];
 
   homebrew.masApps = lib.filterAttrs (name: _ : !lib.elem name excludeApps) {
-    # "CARROT Weather" = 993487541;
-    # "Keystroke Pro" = 1572206224;
+    "CARROT Weather" = 993487541;
+    "Fantastical" = 975937182;
+    "Flighty" = 1358823008;
+    "Keystroke Pro" = 1572206224;
     # "Xcode" = 497799835;
   };
 
